0ae8e0ba367cd4fc2b9ad3a0d21fdfddc0300c48
kieranwatt <kieranmwatt@gmail.com>
2023-11-26 23:30:11 -0500

    SUNDAY GRIND!!!!!!

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,3 +1,22 @@
-import '../styles/style.css'
+import "../styles/style.css";
+import { domselectors } from "./selectors";
+import { sillybladees } from "./array";
 
-document
+/* const store = function(sillybladees) {
+    domselectors.cards.insertAdjacentHTML("beforeend", 
+    '<div class="card" id="card" ')
+} */
+const store = function(sillybladees) {
+    domselectors.cards.insertAdjacentHTML("beforeend",
+    ` <div class="card" id="card">
+      <img src="${sillybladees.img}" alt="img for ${sillybladees.title}." class="card-img">
+      <h4 class="card-title">${sillybladees.title}</h4>
+      <h4 class="card-type">${sillybladees.type}</h4>
+      <h5 class="card-desc">${sillybladees.imgdesc}</h5>
+      <h5 class="card-dabloons">${sillybladees.dabloons}</h5>
+      <h6 class="card-kieranrating">$${sillybladees.kieranrating}</h6>
+      </div>
+      `)
+};
+console.log(sillybladees);
+sillybladees.forEach((card)=>store(card));
---
+++ vite-project/js/selectors.js
@@ -0,0 +1,11 @@
+const domselectors = {
+    cards: document.getElementById("cards"),
+    looks: document.getElementById("looks"),
+    item: document.getElementById("item"),
+    structure: document.getElementById("structure"),
+    hightolowrate: document.getElementById("hightolowrate"),
+    lowtohighrate: document.getElementById("lowtohighrate"),
+    hightolowprice: document.getElementById("hightolowprice"),
+    lowtohighprice: document.getElementById("lowtohighprice"),
+}
+export {domselectors};
